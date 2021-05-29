50617e7d6c6d563ded6710ac13211ebcb7fe5339
Danilka108 <danilka108@yandex.ru>
2021-05-29 12:55:47 +0500

    client: update dirs structure

--- apps/voice-chat/src/app/app.module.ts
+++ apps/voice-chat/src/app/app.module.ts
@@ -1,7 +1,7 @@
 import { NgModule } from '@angular/core'
 import { BrowserModule } from '@angular/platform-browser'
 import { AppComponent } from './app.component'
-import { AppRoutingModule } from './routing/app-routing.module'
+import { AppRoutingModule } from '../routing/app-routing.module'
 import { BrowserAnimationsModule } from '@angular/platform-browser/animations'
 
 @NgModule({
